4515ab7ab273f416669533681a3f57a074d4f50b
NISHANTH S <110479434+nish333@users.noreply.github.com>
2024-05-30 18:52:23 +0530

    Update JwtAuthenticationEntryPoint.java

--- Bank_Application/src/main/java/com/MPKA_Group/demo/config/JwtAuthenticationEntryPoint.java
+++ Bank_Application/src/main/java/com/MPKA_Group/demo/config/JwtAuthenticationEntryPoint.java
@@ -10,15 +10,11 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-
 @Component
-public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint 
-{
-	@Override
-	public void commence(HttpServletRequest request, HttpServletResponse response,
-			AuthenticationException authException) throws IOException, ServletException 
-	{
-		response.sendError(HttpServletResponse.SC_UNAUTHORIZED,authException.getMessage());
-	}
-
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+                         AuthenticationException authException) throws IOException, ServletException {
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED, authException.getMessage());
+    }
 }
